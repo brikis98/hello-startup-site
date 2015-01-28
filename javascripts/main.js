97b38b012e39140641a651baa3da6c15bf3ffc65
Yevgeniy Brikman <brikis98@gmail.com>
2015-01-27 22:33:01 -0800

    Add tracking for links

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
-console.log('This would be the main JS file.');
+(function($, googleAnalytics) {
+  "use strict";
+
+  var trackOutboundLink = function(event) {
+    var anchor = $(event.currentTarget);
+    var url = anchor.attr('href');
+    var trk = anchor.attr('data-trk');
+    var target = anchor.attr('target');
+
+    googleAnalytics.push(['_trackEvent', 'outbound-link', trk, url]);
+
+    if (target !== "_blank") {
+      event.preventDefault();
+      googleAnalytics.push(function() {
+        window.location = url;
+      });
+    }
+  };
+
+  $('.tracked').on('click', trackOutboundLink);
+})($, _gaq);
