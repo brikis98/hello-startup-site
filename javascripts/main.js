1deb6f3313d0020062c2720ffc140493f8e54ce8
Yevgeniy Brikman <brikis98@gmail.com>
2015-01-29 00:43:29 -0800

    Redesign

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,12 @@
 (function() {
   "use strict";
 
+  var scrollSmoothly = function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  };
+
   var trackOutboundLink = function(event) {
     var anchor = $(event.currentTarget);
     var url = anchor.attr('href');
@@ -24,5 +30,35 @@
     ga('send', props);
   };
 
+  var changeHeaderOn = 50;
+  var nav = $('.navbar');
+  var navCollapse = $('.navbar-collapse');
+  var NAVBAR_CLASS_DEFAULT = "navbar-default";
+  var NAVBAR_CLASS_INVERSE = "navbar-inverse";
+
+  var showDefaultNav = function() {
+    nav.removeClass(NAVBAR_CLASS_INVERSE);
+    nav.addClass(NAVBAR_CLASS_DEFAULT);    
+  };
+
+  var showDarkNav = function() {
+    nav.removeClass(NAVBAR_CLASS_DEFAULT);
+    nav.addClass(NAVBAR_CLASS_INVERSE);
+  };
+
+  var invertNav = function(event) {
+    var top = $(window).scrollTop();
+    var isExpanded = navCollapse.attr('aria-expanded') === "true";
+    if (isExpanded || top >= changeHeaderOn) {
+      showDarkNav();
+    } else {
+      showDefaultNav();
+    }
+  };
+
   $('.tracked').on('click', trackOutboundLink);
+  $('a.page-scroll').on('click', scrollSmoothly);
+  navCollapse.on('show.bs.collapse', showDarkNav);
+  navCollapse.on('hidden.bs.collapse', showDefaultNav);
+  $(window).on('scroll', invertNav);
 })();
