406f0826d08ba41f42e7b8e1e1b5ab40d31d03ef
Yevgeniy Brikman <brikis98@gmail.com>
2016-11-17 17:28:14 +0000

    Disable click tracking if GA is disabled

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,7 +17,17 @@
     event.preventDefault();
   };
 
+  // Some people block Google Analytics (e.g. with AdBlock), so we need to check if it's working, or our links will
+  // be broken.
+  var isGoogleAnalyticsWorking = function() {
+    return typeof ga == 'function' && ga.hasOwnProperty('loaded') && ga.loaded === true;
+  };
+
   var trackOutboundLink = function(event) {
+    if (!isGoogleAnalyticsWorking()) {
+      return true;
+    }
+
     var anchor = $(event.currentTarget);
     var url = anchor.attr('href');
     var trk = anchor.attr('data-trk');
